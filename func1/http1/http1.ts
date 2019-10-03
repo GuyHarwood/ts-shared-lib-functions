efb0f700a59bcf2fe4976cee6cb5b77969509b3b
Guy Harwood <guy@milkchip.com>
2019-10-03 15:26:11 +0100

    working with function outputs in root

--- func1/http1/http1.ts
+++ func1/http1/http1.ts
@@ -1,21 +1,13 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { AzureFunction, Context } from "@azure/functions"
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+const httpTrigger: AzureFunction = async function (context: Context): Promise<void> {
     context.log('HTTP trigger function processed a request.');
-    const name = (req.query.name || (req.body && req.body.name));
 
-    if (name) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
-        };
-    }
-    else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
-    }
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: `Hello from the http1 function in the root dist folder 👋🏼`
+    };
+
 };
 
 export default httpTrigger;
