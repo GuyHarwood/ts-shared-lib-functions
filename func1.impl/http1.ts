52ae2b47b38149ddbcc842ec8381c12e48062243
Guy Harwood <guy@milkchip.com>
2019-10-04 08:21:31 +0100

    issues

--- func1.impl/http1.ts
+++ func1.impl/http1.ts
@@ -1,14 +1,14 @@
 import { AzureFunction, Context } from "@azure/functions"
-import * as tslib from '@tslib/HelloService'
+import { HelloService } from '@tslib/index'
 
 const httpTrigger: AzureFunction = async function (context: Context): Promise<void> {
     context.log('HTTP trigger function processed a request.');
-    const svc = new tslib.HelloService()
+    const svc = new HelloService()
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: svc.say('http1')
     };
-
+    
 };
 
 export default httpTrigger;
